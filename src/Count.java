c42f69fb1f89911e75f5753a0ddab3ded86fd330
Nathan876 <sequins.trancheur0c@icloud.com>
2025-03-12 23:34:22 +0100

    Rendu projet

--- src/Count.java
+++ src/Count.java
@@ -1,16 +1,16 @@
 public class Count {
     public static String selectedItem = "carrot";
     public static int count = 0;
-    public static int carrots = 10;
-    public static int corn = 10;
-    public static int wheat = 10;
-    public static int chickens = 10;
-    public static int cows = 10;
+    public static int carrots = 0;
+    public static int corn = 0;
+    public static int wheat = 0;
+    public static int chickens = 0;
+    public static int cows = 0;
     public static int eggs = 0;
     public static int milk = 0;
     public static int money = 500;
-    public static int wool = 10;
-    public static int sheep = 10;
+    public static int wool = 0;
+    public static int sheep = 0;
     public static int row = 5;
     public static int col = 5;
     public static boolean isExpand = false;
